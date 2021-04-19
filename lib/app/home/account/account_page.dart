f5dd1e0e53ef13e1245ac5fdaa5d0dde524a089a
Karim Elbayaa <kareemelbayaa@gmail.com>
2021-04-19 03:16:32 +0200

    added circular avatar to the account_page.dart

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -1,17 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/common_widgets/avatar.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context,listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Account'),
         actions: [
           FlatButton(onPressed: ()=>_confirmSignOut(context), child: Text('Logout',style: TextStyle(fontSize: 18.0,color: Colors.white),))
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(130),
+          child: _buildUserInfo(auth.currentUser),
+        ),
       ),
     );
   }
@@ -33,5 +40,12 @@ class AccountPage extends StatelessWidget {
     if (didRequestSignOut == true) {
       _signout(context);
     }
+  }
+
+  Widget _buildUserInfo(User currentUser) {
+    return Avatar(
+      photoUrl: currentUser.photoURL,
+      radius: 50,
+    );
   }
 }
